7eb0283dbafae39a9640a4427b47d44db81adaef
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2024-01-26 19:34:23 -0700

    lol

---
+++ Hangman/main.py
@@ -0,0 +1,8 @@
+import os, random
+def cls():
+  os.system('cls' if os.name == 'nt' else 'clear')
+from hangman_words import word_list
+from hangman_art import *
+print(logo)
+word = random.choice(word_list)
+print("_" * len[word])
