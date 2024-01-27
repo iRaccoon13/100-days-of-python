9bdad5271de87d1fd174ef5e6827424b30d59c90
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2024-01-27 18:49:35 +0000

    hangman done

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,13 +1,71 @@
-import os, random
+from hangman_art import logo, stages
+from hangman_words import word_list
+import os
+import random
+from time import time
 
-
+# Clear console
 def cls():
   os.system('cls' if os.name == 'nt' else 'clear')
-
-
-from hangman_words import word_list
-from hangman_art import *
-
-print(logo)
+def nothing():
+  pass
 word = random.choice(word_list)
-print("_" * len[word])
+lives = 6
+display = []
+lettersguessed = []
+wordguessed = False
+firstguess = True
+formatted_display = ""
+while lives > 0 and not wordguessed:
+  cls()
+  print(logo)
+  print(f"Your word is {len(word)} letters long:")
+  livespunc = "!" if lives < 3 else "."
+  print("_" * len(word)) if firstguess else print(formatted_display)
+  while True:
+    guess = input(f"\nGuess?- {lives} lives left{livespunc} \n:").lower()
+    if len(guess) == 1:
+      break
+    else:
+      cls()
+      print(logo)
+      print("Please enter a single letter.")
+      input("Press enter to continue. ")
+  if guess in lettersguessed:
+    cls()
+    print(logo)
+    print(f"You already guessed {guess}!")
+    input("Press enter to continue. ")
+  else:
+    firstguess = False
+    #check guess
+    try: 
+      word.index(guess)
+    except ValueError:
+      lives -= 1
+      if lives == 0:
+        break
+      cls()
+      print(logo)
+      print(f"Wrong! {lives} lives left{livespunc}\nPress enter to try again!\n")
+      input(stages[lives])
+      lettersguessed.append(guess)
+    else:
+      cls()
+      print(logo)
+      print(f"Correct! {lives} lives left{livespunc}\nPress enter to try again!")
+      lettersguessed.append(guess)
+      input()
+    #check if word is guessed and setup display
+    display = []
+    for i in word:
+      display.append("_") if i not in lettersguessed else display.append(i)
+    formatted_display = ""
+    for i in display:
+      formatted_display += i
+    wordguessed = bool("_" not in display)
+if wordguessed:
+  print(f"You won! The word was {word}!")
+else:
+  print(f"You lost! The word was {word}!")
+  input("Press enter to go home. ")
---
+++ main.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(["python", "Hangman/main.py"])
