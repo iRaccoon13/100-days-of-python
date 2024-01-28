e734a2d8413ef326eb619dc00051fa4cc953e55a
iRaccoon13 <eddie.reasoner@gmail.com>
2024-01-28 08:55:30 -0700

    guess the number started

---
+++ GuessTheNumber/main.py
@@ -0,0 +1,6 @@
+from time import time
+import random, os, sys
+from 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from loading import loading_screen
+
--- main.py
+++ main.py
@@ -1,21 +1,71 @@
-import subprocess
-games = ["Exit", "Hangman","Tic Tac Toe","Rock Paper Scissors","Guess The Number"]
-gamemenu = """"""
-gamemenu += "_"*len(max(games, key=len))
-gamemenu += "_"*10+"\n"
-for i in games:
-  gamemenu += f"| {games.index(i)}. |{i}"
-  gamemenu += " "*(len(max(games, key=len))-len(i)) + "  |\n"
-gamemenu += ("^"*len(max(games, key=len)+"  |"+ "_"*10))+ "\n"
-while True:
-  print(gamemenu)
-  choice = input("What would you like to play? \nEnter the number. \n\n: ")
-  None if choice.isdigit() else print("Invalid input")
-  files = {
-    "Hangman": "Hangman/main.py",
-    "Tic Tac Toe": "TicTacToe/main.py",
-    "Rock Paper Scissors": "rps/main.py",
-    "Guess The Number": "GuessTheNumber/main.py"
-}
-  exit(0) if int(choice) == 0 else False
-  subprocess.run(["python", files[games[int(choice)]]])
+from hangman_art import logo, stages
+from hangman_words import word_list
+import os,random,sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from loading import loading_screen
+#from time import time
+loading_screen(logo,500)
+# Clear console
+def cls():
+  os.system('cls' if os.name == 'nt' else 'clear')
+def nothing():
+  pass
+word = random.choice(word_list)
+lives = 6
+display = []
+lettersguessed = []
+wordguessed = False
+firstguess = True
+formatted_display = ""
+while lives > 0 and not wordguessed:
+  cls()
+  print(logo)
+  livespunc = "!" if lives < 3 else "."
+  print("_ " * len(word)) if firstguess else print(formatted_display)
+  guess_allowed = False
+  while not guess_allowed:
+    guess = input(f"\nGuess?- {lives} lives left{livespunc} \n\n:").lower()
+    if len(guess) == 1 and not guess.isdigit():
+      guess_allowed = True
+    else:
+      cls()
+      print(logo)
+      print("Please enter a single letter.")
+      input("Press enter to continue. ")
+      cls()
+  if guess in lettersguessed:
+    cls()
+    print(logo)
+    print(f"You already guessed {guess}!")
+    input("Press enter to continue. ")
+  else:
+    firstguess = False
+    #check guess
+    try: 
+      word.index(guess)
+    except ValueError:
+      lives -= 1
+      cls()
+      print(logo)
+      print(f"Wrong! {lives} lives left{livespunc}\nPress enter to try again!\n")
+      input(stages[lives])
+      lettersguessed.append(guess)
+    else:
+      cls()
+      print(logo)
+      print(f"Correct! {lives} lives left{livespunc}\nPress enter to continue!")
+      lettersguessed.append(guess)
+      input()
+    #check if word is guessed and setup display
+    display = []
+    for i in word:
+      display.append("_ ") if i not in lettersguessed else display.append(i)
+    formatted_display = ""
+    for i in display:
+      formatted_display += i
+    wordguessed = True if formatted_display == word else False
+if wordguessed:
+  print(f"You won! The word was {word}!")
+else:
+  print(f"You lost! The word was {word}!")
+input("Press enter to go home. ")
