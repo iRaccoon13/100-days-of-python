45a90a62c9f5c2b43665201c78c6ab8803e5f878
iRaccoon13 <eddie.reasoner@gmail.com>
2024-01-28 10:52:48 -0700

    GTN but its broken will fix later

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -1,6 +1,23 @@
 from time import time
 import random, os, sys
-from 
+from GuessTheNumberArt import logo
+import GuessTheNumberModeSpecific as modeSpecific
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from loading import loading_screen
-
+loading_screen(logo, 500)
+while True:
+  print("""
+______________________
+| 1. | Free Play     |
+| 2. | Timed         |
+| 3. | Limited Tries |
+^^^^^^^^^^^^^^^^^^^^^^""")
+  mode = input("What mode would would you like to play? Input the number from the table. \n\n: ")
+  try:
+    int(mode)
+  except: 
+    print("Invalid input. ")
+  else: 
+    mode = int(mode)
+    break
+lives = 999999 if mode == 1 else lives = 10
--- loading.py
+++ loading.py
@@ -2,10 +2,11 @@ def loading_screen(logo,delay):
   from time import sleep
   from os import system,name
   A = "#"
-  B = " "
-  for i in range(0,10):
+  B = "."
+  for i in range(0,11):
     system('cls' if name == 'nt' else 'clear')
     Bs = 10-i
     print(logo)
+    print("loading...")
     print(f"[{A*i}{B*Bs}]")
     sleep(delay/1000)
